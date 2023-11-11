9fea5e565252693b2221215171783f1e5ff6c064
Say H <chse22036@g.nihon-u.ac.jp>
2023-11-11 12:22:48 +0900

    ***

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 
 const images = [
-  { src: '/images/pic1.jpg', alt: 'Closeup of a human eye' },
-  { src: '/images/pic2.jpg', alt: 'Rock that looks like a wave' },
-  { src: '/images/pic3.jpg', alt: 'Purple and white pansies' },
-  { src: '/images/pic4.jpg', alt: "Section of wall from a pharoah's tomb" },
-  { src: '/images/pic5.jpg', alt: 'Large moth on a leaf' },
+  { src: "/images/pic1.jpg", alt: "Closeup of a human eye" },
+  { src: "/images/pic2.jpg", alt: "Rock that looks like a wave" },
+  { src: "/images/pic3.jpg", alt: "Purple and white pansies" },
+  { src: "/images/pic4.jpg", alt: "Section of wall from a pharoah's tomb" },
+  { src: "/images/pic5.jpg", alt: "Large moth on a leaf" },
 ];
 
 export default function App() {
