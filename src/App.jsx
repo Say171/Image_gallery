8e37e0808964abb5d76d20ddbbf471b96948dc05
Say H <chse22036@g.nihon-u.ac.jp>
2023-11-29 17:50:13 +0900

    ***

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import React, { useState } from 'react';
 export default function App() {
 
   const images = [
-    { src: "./images/pic1.jpg", alt: "Closeup of a human eye" },
-    { src: "./images/pic2.jpg", alt: "Rock that looks like a wave" },
-    { src: "./images/pic3.jpg", alt: "Purple and white pansies" },
-    { src: "./images/pic4.jpg", alt: "Section of wall from a pharoah's tomb" },
-    { src: "./images/pic5.jpg", alt: "Large moth on a leaf" },
+    { src: "images/pic1.jpg", alt: "Closeup of a human eye" },
+    { src: "images/pic2.jpg", alt: "Rock that looks like a wave" },
+    { src: "images/pic3.jpg", alt: "Purple and white pansies" },
+    { src: "images/pic4.jpg", alt: "Section of wall from a pharoah's tomb" },
+    { src: "images/pic5.jpg", alt: "Large moth on a leaf" },
   ];
 
    // ステートの宣言と初期化
